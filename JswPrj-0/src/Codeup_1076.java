4ff95bdbdd2b08a429a58c95c3fb55d1020cfb3e
chiriem <maoru23@gmail.com>
2021-04-13 09:42:17 +0900

    1076

---
+++ JswPrj-0/src/Codeup_1076.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Codeup_1076 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		char ch = sc.next().charAt(0);
+		
+		for (char i = 'a'; i <= ch; i++) {
+			System.out.print(i+" ");
+		}
+	}
+}
