c7f166e1889443ed960c0cf3ffdfca5a5136fe1d
chiriem <maoru23@gmail.com>
2021-04-14 08:50:14 +0900

    1077

---
+++ JswPrj-0/src/Codeup_1077.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Codeup_1077 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int num = sc.nextInt();
+		
+		for (int i=0;i<=num;i++) {
+			System.out.println(i);
+		}
+	}
+}
