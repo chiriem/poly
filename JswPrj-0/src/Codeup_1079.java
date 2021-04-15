e96b40226edcf19a3beea2a34474c76b910224f7
chiriem <maoru23@gmail.com>
2021-04-16 08:34:26 +0900

    1079

---
+++ JswPrj-0/src/Codeup_1079.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Codeup_1079 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		while(true) {
+			char a = sc.next().charAt(0);
+			
+			System.out.println(a);
+			
+			if (a == 'q')
+				break;
+		}
+	}
+
+}
