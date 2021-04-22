f18cabffe874d2ac786552605577bbcb43174a11
chiriem <maoru23@gmail.com>
2021-04-23 08:40:02 +0900

    1085

---
+++ JswPrj-0/src/Codeup_1085.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Codeup_1085 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int h = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		int s = sc.nextInt();
+		
+		float num = (float)h*b*c*s/8/1024/1024;
+		
+		System.out.printf("%.1f MB",num);		
+	}
+
+}
