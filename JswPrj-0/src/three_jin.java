eff3214b3274280a39340c0a63bce958bdff12a6
chiriem <maoru23@gmail.com>
2021-05-14 08:37:37 +0900

    3진법 뒤집기

---
+++ JswPrj-0/src/three_jin.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class three_jin {
+
+   public int solution(int n) {
+      int answer = 0;
+      int m = n;
+      int count = 1;
+      while (m > 2) {
+         m = m/3;
+         count++;
+      }
+      int [] thr = new int [count];
+      m = n;
+      for (int i = 0; i < count; i++) {
+         thr[i] = m % 3;
+         m = m/3;
+      }
+      int [] ten = new int [count];
+      int cal = 1;
+      for (int i = 0; i < count; i++) {
+         for (int j = 0; j < count - 1 -i; j++) {
+            cal = 3*cal;
+         }
+         ten[i] = thr[i]*cal;
+         cal = 1;
+      }
+     
+      for (int i = 0; i < count; i++) {
+         answer += ten[i];
+      }
+      return answer;
+   }
+   public static void main(String[] args) {
+      three_jin tj = new three_jin();
+      Scanner sc = new Scanner(System.in);
+      
+      int in = sc.nextInt();
+      
+      int ou = tj.solution(in);
+      
+      System.out.println(ou);
+   }
+}
