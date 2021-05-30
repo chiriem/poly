648fd541065f9850154722b354b484c729fa5265
chiriem <maoru23@gmail.com>
2021-05-31 08:42:05 +0900

    프로그래머스 짝수와 홀수

---
+++ JswPrj-0/src/Even_n_Odd.java
@@ -0,0 +1,19 @@
+
+public class Even_n_Odd {
+	 public String solution(int num) {
+	        String answer = "";
+	        
+	        if (num % 2 == 0){
+	            answer = "Even";
+	        } else {
+	            answer = "Odd";
+	        }
+	        
+	        return answer;
+	   }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
