f3912e08a41e575cff736488f6acb1c7b43e3b29
chiriem <maoru23@gmail.com>
2021-06-11 08:33:30 +0900

    배열 합 추가

---
+++ JswPrj-0/src/arr_sum.java
@@ -0,0 +1,14 @@
+class arr_sum {
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        int[][] answer = new int [arr1.length][arr1[1].length];
+        
+        for (int i = 0; i < arr1.length; i++){
+            for (int k = 0; k < arr1[i].length; k++){
+                answer[i][k] = arr1[i][k] + arr2[i][k];
+            }
+        }
+        
+        
+        return answer;
+    }
+}
